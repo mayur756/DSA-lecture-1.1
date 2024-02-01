0c2f4c7668a4072e544135b2b6cd3111a9cd475d
mayur756 <147247475+mayur756@users.noreply.github.com>
2024-02-01 20:07:39 +0530

    first upload

---
+++ sum.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	int no1,no2;
+	
+	cout<<"enter the no1:";
+	cin>>no1;
+	
+	cout<<"enter the no2:";
+	cin>>no2;
+	
+	cout<<"sum is:"<<no1+no2;
+	
+	return 0;
+}
